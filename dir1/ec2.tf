82ddc02875c157434170d723f208cfcd1c107638
jiseon.kim <jiseon.kim@bespinglobal.com>
2022-10-19 14:57:38 +0900

    Update subnet_id

--- dir1/ec2.tf
+++ dir1/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "dir1" {
   ami           = data.aws_ami.amazon-common-ami.id
   instance_type = "t3.micro"
 
-  subnet_id = "subnet-0f0d20e44d0cf905a"
+  subnet_id = "subnet-0dd1964a2d73937cd"
 
   tags = {
     Name = "DIR-111"
