9d081a5295b1894a1fcc413dfc7f6d59b472d790
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2022-10-27 14:03:25 +0900

    Update ec2.tf

--- dir1/ec2.tf
+++ dir1/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "dir1" {
   ami           = data.aws_ami.amazon-common-ami.id
   instance_type = "t3.micro"
 
-  subnet_id = "subnet-0dd1964a2d73937cd"
+  subnet_id = "subnet-1af11756"
 
   tags = {
     Name = "DIR-111"
